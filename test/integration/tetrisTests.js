341f9763a88289a502618bbf9a114b231a48a492
joe bloggs <me@email.com>
2016-01-19 10:53:40 +1100

    setting up testing

--- test/integration/tetrisTests.js
+++ test/integration/tetrisTests.js
@@ -3,8 +3,18 @@ if (typeof QUnit == 'undefined') // if your tests also run in the browser...
 
 var TetrisGame = require("../../lib/tetris")
 var Grid = require("../../lib/grid")
+//var LongBar = require("../../lib/pieces/longBar")
 var tetrisGame =new TetrisGame(new Grid(10, 10), 1);
 
 QUnit.test("Tetris Game should initiate witha queue of 4 pieces", function( assert ) {
     assert.equal(4, tetrisGame.pieceQueue.length , "failed!" );
+});
+
+
+QUnit.test("it should not allow to a piece to rotate clockwise if it falls off the grid", function (assert) {
+    var grid = new Grid(5,5);
+    var piece = new LongBar(4,0,0);
+    var gameController = new TetrisGame(grid, 0, piece);
+    var result = gameController.tryMove(piece, {move: 'rotateClockwise', x_transformation: 0, y_transformation: 0});
+    assert.equal(false, result, "failed");
 });
--- test/unit/gamePiecesTests.js
+++ test/unit/gamePiecesTests.js
@@ -8,13 +8,17 @@ var basePiece = new BasePiece(10, 10);
 var LeftDownwardBar = require("../../lib/pieces/leftDownwardBar")
 var leftDownwardBar = new LeftDownwardBar(10, 10);
 
+var LongBar = require("../../lib/pieces/longBar")
+var longBar = new LongBar(10, 10);
 
 QUnit.test( "LeftDownwardBar name", function( assert ) {
     assert.equal("LeftDownwardBar", leftDownwardBar.name, "failed!" );
-    assert.equal(10, leftDownwardBar.x_coord,  "failed!" );
 });
 
 QUnit.test( "LeftDownwardBar x_coord", function( assert ) {
-    assert.equal("LeftDownwardBar", leftDownwardBar.name, "failed!" );
     assert.equal(10, leftDownwardBar.x_coord,  "failed!" );
+});
+
+QUnit.test( "LongBar properties", function( assert ) {
+    assert.equal(10, longBar.x_coord,  "failed!" );
 });
