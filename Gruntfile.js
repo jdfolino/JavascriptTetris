b7d63f6fd3c94d2b7900a09822585cb221c3341f
joe bloggs <me@email.com>
2016-01-21 16:05:55 +1100

    browserify now in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,20 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-
+        browserify: {
+            client: {
+                src: ['lib/tetris.js'],
+                dest: 'target/tetris.min.js',
+                options: {
+                    browserifyOptions: {
+                        standalone: 'Tetris'
+                    }
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-browserify');
     //browserify lib/tetris.js -s Tetris > target/tetris-min.js
 
 };
